c4822e8f5f3f0a4afbdcc23ca7e35763aa053b09
AmOchoat <am.ochoat@uniandes.edu.co>
2022-09-24 10:28:30 -0500

    set up

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import find_packages, setup
 
 setup(
-<<<<<<< HEAD
     name='codefestadastra',
     packages=find_packages(include=['codefestadastra']),
     version='1.0.0',
@@ -9,12 +8,3 @@ setup(
     author='Me',
     license='MIT',
 )
-=======
-    name="codefestadastra",
-    packages=find_packages(include=["codefestadastra"]),
-    version="0.1.0",
-    description="My first Python library",
-    author="Me",
-    license="MIT",
-)
->>>>>>> 21ed58d0000d4498cc467e06f0ecd3c9e07ab3c8
