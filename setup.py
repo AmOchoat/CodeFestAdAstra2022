b3a1d4be3c1a1d4a632729c48da1bb310adcc933
Geisson19 <gs.ponce20@uniandes.edu.co>
2022-09-24 10:08:16 -0500

    refactoring and documentation

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import find_packages, setup
+
 setup(
-    name='codefestadastra',
-    packages=find_packages(include=['codefestadastra']),
-    version='0.1.0',
-    description='My first Python library',
-    author='Me',
-    license='MIT',
+    name="codefestadastra",
+    packages=find_packages(include=["codefestadastra"]),
+    version="0.1.0",
+    description="My first Python library",
+    author="Me",
+    license="MIT",
 )
